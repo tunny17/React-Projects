ef47c0215cd8ec8983f8f874a931002fcee13740
tunny17 <tvnji17@gmail.com>
2022-11-02 10:47:33 +0100

    anonymous functions

--- src/App.js
+++ src/App.js
@@ -3,16 +3,37 @@ import './App.scss';
 
 
 function App() {
-  const [count, setCount] = useState(5);
+  const [count, setCount] = useState(0);
+
+  const handleSubtract = () => {
+    setCount(count - 1);
+  };
+  const handleReset = () => {
+    setCount(0);
+  };
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+
+  let color = '#fff';
+  if (count >= 1) {
+    color = 'yellow'
+  } else if (count < 0 ) {
+    color = 'red'
+  } else {
+    color = 'white'
+  }
+
+
   return (
     <section  className="counter-sec  --flex-center">
       <div className="container counter --card  --center-all">
         <h1 className="--text-light">React Counter App</h1>
-        <p  className="count  --my2 --text-md --text-light">{count}</p>
+        <p  className="count  --my2 --text-md --text-light  --fw-bold" style={{color: color}}>{count}</p>
         <div className="buttons">
-          <button className="--btn  --btn-danger">- Subtract</button>
-          <button className="--btn">! Reset</button>
-          <button className="--btn  --btn-success">+ Add</button>
+          <button className="--btn  --btn-danger" onClick={handleSubtract}>- Subtract</button>
+          <button className="--btn" onClick={handleReset}>! Reset</button>
+          <button className="--btn  --btn-success"  onClick={handleAdd}>+ Add</button>
         </div>
       </div>
     </section>
