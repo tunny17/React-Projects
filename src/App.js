4f95c85df461bbec187862b11a08f41f2c32b746
tunny17 <tvnji17@gmail.com>
2022-11-02 10:24:54 +0100

    logic base

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from 'react';
+import './App.scss';
+
 
 function App() {
+  const [count, setCount] = useState(5);
   return (
-    <section  className="counter-sec">
-      <div className="container counter">
-        <h1>React Counter App</h1>
+    <section  className="counter-sec  --flex-center">
+      <div className="container counter --card  --center-all">
+        <h1 className="--text-light">React Counter App</h1>
+        <p  className="count  --my2 --text-md --text-light">{count}</p>
+        <div className="buttons">
+          <button className="--btn  --btn-danger">- Subtract</button>
+          <button className="--btn">! Reset</button>
+          <button className="--btn  --btn-success">+ Add</button>
+        </div>
       </div>
     </section>
   )
