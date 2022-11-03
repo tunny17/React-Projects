ddee8855bfb286066c7da362438348bd3f88f3c7
tunny17 <tvnji17@gmail.com>
2022-11-03 21:04:55 +0100

    logic completion for the delete

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -6,9 +6,15 @@ import { FaTrashAlt } from 'react-icons/fa'
 const Profile = () => {
     const [userProfile, setUserProfile] = useState(profileData);
 
+    const removeProfile = (id) => {
+        console.log(id);
+        const newProfileList = userProfile.filter((profile) => profile.id !== id)
+        setUserProfile(newProfileList);
+    }
+    
 
   return (
-    <section    className='profile-sec'>
+    <section    className='profile-sec  --flex-center'> 
         <div className="container">
             <h2 className='--text-light'>Profile App</h2>
             {userProfile.map((profile) => (
@@ -18,7 +24,7 @@ const Profile = () => {
                         <h4 className='--text-light'>Name: {profile.name}</h4>
                         <p  className='--text-light'>Job: {profile.job}</p>
                     </div>
-                    <FaTrashAlt size={18}   className="icon"/>
+                    <FaTrashAlt size={18}   className="icon"    onClick={() => removeProfile(profile.id)}/>
                 </div>
             ))}
         </div>
