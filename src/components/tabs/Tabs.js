96d4e6e20d08aeb2afdaa8451ace3d7fac08c7c8
tunny17 <tvnji17@gmail.com>
2022-11-06 19:23:11 +0100

    styling completion

--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -1,25 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
 import './Tabs.scss'
 
 const Tabs = () => {
   return (
-    <section    className='tabs-section'>
+    <section    className='tabs-section     --100vh     --flex-center'>
       <div className="container">
 
         <div className="tabs-heading    --text-center">
             <h2 className='--line'>More About Us</h2>
-            <p>Click the Tab buttons to learn more.</p>
+            <p  className='--text-sm'>Click the Tab buttons to learn more.</p>
         </div>
 
-        <div className="tabs">
+        <div className="tabs    --flex-start  --bg-light">
             <div className="tabs-title">
-                <button className='tab'>Who We Are</button>
+                <button className='tab  active-tab'>Who We Are</button>
                 <button className='tab'>What We Do</button>
                 <button className='tab'>Get In Touch</button>
             </div>
 
             <div className="tabs-content">
-                <article className="content">
+                <article className="content active-content">
                     <h4>Who We Are</h4>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor sint sit distinctio. Dolor quam ipsum veritatis! Porro similique veniam obcaecati?</p>
                 </article>
