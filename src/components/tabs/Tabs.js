7cad4c87c994b5b937cbce6d0d76c643f49a33ee
tunny17 <tvnji17@gmail.com>
2022-11-06 18:58:47 +0100

    completion of the jsx

--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -5,7 +5,34 @@ const Tabs = () => {
   return (
     <section    className='tabs-section'>
       <div className="container">
-        
+
+        <div className="tabs-heading">
+            <h2>More About Us</h2>
+            <p>Click the Tab buttons to learn more.</p>
+        </div>
+
+        <div className="tabs">
+            <div className="tabs-title">
+                <button className='tab'>Who We Are</button>
+                <button className='tab'>What We Do</button>
+                <button className='tab'>Get In Touch</button>
+            </div>
+
+            <div className="tabs-content">
+                <article className="content">
+                    <h4>Who We Are</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor sint sit distinctio. Dolor quam ipsum veritatis! Porro similique veniam obcaecati?</p>
+                </article>
+                <article className="content">
+                    <h4>Who We Are</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor sint sit distinctio. Dolor quam ipsum veritatis! Porro similique veniam obcaecati?</p>
+                </article>
+                <article className="content">
+                    <h4>Who We Are</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor sint sit distinctio. Dolor quam ipsum veritatis! Porro similique veniam obcaecati?</p>
+                </article>
+            </div>
+        </div>
       </div>
     </section>
   )
