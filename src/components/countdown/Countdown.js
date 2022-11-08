b2527b49ba06dc809183bae08480f468fd261a54
tunny17 <tvnji17@gmail.com>
2022-11-08 19:36:24 +0100

    styling completion

--- src/components/countdown/Countdown.js
+++ src/components/countdown/Countdown.js
@@ -3,8 +3,33 @@ import './Countdown.scss'
 
 const Countdown = () => {
   return (
-    <div>
-      <h1>Countdown</h1>
+    <div  className='countdown-section'>
+      <div className="container --flex-between">
+        <h2 className='--text-md'>Christmas Sale</h2>
+
+        <div className="countdown --card  --my">
+          <time className="--flex-center  --text-center --color-white">
+            
+            <span>
+              <p>00</p>
+              <small>Days</small>
+            </span>
+            <span>
+              <p>01</p>
+              <small>Hours</small>
+            </span>
+            <span>
+              <p>20</p>
+              <small>Minutes</small>
+            </span>
+            <span>
+              <p>00</p>
+              <small>Seconds</small>
+            </span>
+          </time>
+        </div>
+      </div>
+      <p  className='close'>X</p>
     </div>
   )
 }
