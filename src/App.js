d0a8694773be05dfc37130004c5452d6865d0b77
tunny17 <tvnji17@gmail.com>
2022-11-08 20:38:01 +0100

    END OF LOGIC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import {useState} from 'react'
 import './App.scss';
 import Countdown from './components/countdown/Countdown';
 import Product from './components/product/Product';
 
 
 function App() {
+  const [showCountdown, setShowCountdown] = useState(true);
   return (
     <div>
-      <Countdown />
+      {showCountdown && <Countdown onHideCountdown={() => setShowCountdown(false)}/> }
       <Product />
     </div>
   )
