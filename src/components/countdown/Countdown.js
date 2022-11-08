bf60ccef41129e6864f76b8e598dcc11aaa65516
tunny17 <tvnji17@gmail.com>
2022-11-08 20:59:18 +0100

    use effect dependency correction

--- src/components/countdown/Countdown.js
+++ src/components/countdown/Countdown.js
@@ -34,6 +34,10 @@ const Countdown = ({onHideCountdown}) => {
 
     },  1000)
   };
+  
+  useEffect(() => {
+    startCountDown();
+  });
 
 
   function format(t) {
@@ -41,9 +45,6 @@ const Countdown = ({onHideCountdown}) => {
   }
 
 
-  useEffect(() => {
-    startCountDown();
-  }, [])
 
   return (
     <div  className='countdown-section'>
