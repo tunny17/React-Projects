ada22e91760f0b24ec3122c6b4e780b5ff4b9384
tunny17 <tvnji17@gmail.com>
2022-11-23 10:14:56 +0100

    input features

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -2,17 +2,25 @@ import { useState } from 'react'
 import './Weather.scss'
 
 const Weather = () => {
-  const [input, setInput] = useState("")
-  const [weather, setWeather] = useState({})
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(true)
-  const [errorMsg, setErrorMsg] = useState("")
+  let today = new Date();
+  let date =  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+
+  const [input, setInput] = useState("");
+  const [weather, setWeather] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(true);
+  const [errorMsg, setErrorMsg] = useState("");
+
+
+  const [dateToday, setDateToday] = useState(date);
 
 
   const api = {
     url: "https://api.openweathermap.org/data/2.5/",
     key: "7ac67a2b59c0ee2e9ebcc21b1341f523",
-  }
+  };
+
+  const iconURL = "http://openweathermap.org/img/w/";
 
   const getInput = (e) => { 
     setInput(e.target.value)
@@ -24,12 +32,27 @@ const Weather = () => {
       setError(true)
     }
     if (e.key === "Enter" && input !== "") {
-      fetch(`${api.url}weather?q=${input}&unit=metrics&APPID=${api.key}`)
+      setIsLoading(true);
+      setError(true);
+      fetch(`${api.url}weather?q=${input}&unit=metric&APPID=${api.key}`)
       .then((res) => {
+        if (!res.ok) {
+            throw Error("Failed to fetch data")
+        }
         return res.json();
       })
       .then((data) => {
         console.log(data);
+        setWeather(data);
+        setInput("");
+        setError(false);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        setError(true)
+        setErrorMsg(err.message)
+        setIsLoading(false)
       })
     }
   }
@@ -41,26 +64,26 @@ const Weather = () => {
       <div className="container weather --flex-center">
         <div className="weather-app --text-light">
             <h1>Weather App</h1>
-            <p>2022-11-09</p>
+            <p>{dateToday}</p>
 
             <div className="--form-control  --my2">
                 <input type="text"  placeholder='Search city name...' onChange={getInput} value={input} onKeyPress={getWeatherData}/>
             </div>
 
             {error ? (
-                <p>{errorMsg}</p>
+                <p  className={errorMsg != "" ? "error" : ""}>{errorMsg}</p>
             ) : (
               <div className="result  --card  --my2">
-                <h2>Abuja</h2>
+                <h2>{weather.name}, {weather.sys.country}</h2>
                 <div className="icon">
-                    <img src="" alt="clouds" />
+                    <img src={iconURL + weather.weather[0].icon + ".png"} alt={weather.weather[0].main} />
                 </div>
-                <p>Temp: 23°C</p>
-                <p>Weather: Clouds</p>
-                <p>Temp Range: 23°C / 24°C</p>
+                <p>Temp: {Math.round(weather.main.temp)}°C</p>
+                <p>Weather: {weather.weather[0].main}</p>
+                <p>Temp Range: {Math.round(weather.main.temp_min)}°C / {Math.round(weather.main.temp_max)}°C</p>
             </div>
             )}
-
+            {isLoading && <h3>Loading...</h3>}
         </div>
       </div>
     </section>
